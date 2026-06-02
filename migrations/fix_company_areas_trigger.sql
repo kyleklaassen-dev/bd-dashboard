5de5d47412818eec5394d180a1cef24511ffbd59
kyleklaassen-dev <kyleklaassen2@gmail.com>
2026-06-02 08:02:24 -0400

    fix compute_landscape_scores --all flag + trigname typo in migration

--- migrations/fix_company_areas_trigger.sql
+++ migrations/fix_company_areas_trigger.sql
@@ -11,7 +11,7 @@ DECLARE
 BEGIN
   -- Drop any trigger on company_areas that references disease_areas
   FOR trig_name IN
-    SELECT t.trigname
+    SELECT t.tgname
     FROM pg_trigger t
     JOIN pg_class c ON c.oid = t.tgrelid
     JOIN pg_namespace n ON n.oid = c.relnamespace
